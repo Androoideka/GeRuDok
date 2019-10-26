52607a0ab42432e4d4699301be3c980bbfc2ff85
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-10-26 19:00:27 +0000

    Vraćanje rezolucije sistema

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,12 +1,16 @@
 package gui;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
 	private static MainFrame instance = null;
 	
 	private MainFrame() {
-		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
 	}
 	
 	public static MainFrame getInstance() {
