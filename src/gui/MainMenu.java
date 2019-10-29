4e7c9a65f0292a671864153d975cc368318f7480
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-10-29 15:45:08 +0000

    Dodavanje stavki na meni

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,7 +1,20 @@
 package gui;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainMenu extends JMenuBar {
-
+	public MainMenu() {
+		JMenu file = new JMenu("File");
+		JMenu edit = new JMenu("Edit");
+		JMenu help = new JMenu("Help");
+		
+		JMenuItem About = new JMenuItem("About");
+		help.add(About);
+		
+		add(file);
+		add(edit);
+		add(help);
+	}
 }
