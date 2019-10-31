94c6b2144e34c661ebf5aae522fe3980be3bfa12
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-10-31 22:04:43 +0000

    Dodavanje indeksa

--- src/gui/AboutDialog.java
+++ src/gui/AboutDialog.java
@@ -10,15 +10,21 @@ import javax.swing.JPanel;
 public class AboutDialog extends JDialog {	
 	public AboutDialog(MainFrame frame) {
 		super(frame, "About");
-		Dimension dim=new Dimension(50, 20);
+		Dimension dim=new Dimension(30, 20);
 		JLabel manager=new JLabel("Stefan Budimac RN06-2018");
 		JLabel leader=new JLabel("Andrej Gasic RN02-2018");
+		JLabel indexManager=new JLabel("RN06-2018");
+		JLabel indexLeader=new JLabel("RN02-2018");
 		manager.setSize(dim);
 		leader.setSize(dim);
-		GridLayout layout=new GridLayout();
+		indexManager.setSize(dim);
+		indexLeader.setSize(dim);
+		GridLayout layout=new GridLayout(2, 2);
 		JPanel panel=new JPanel(layout);
 		panel.add(manager);
 		panel.add(leader);
+		panel.add(indexManager);
+		panel.add(indexLeader);
 		add(panel);
 		setSize(new Dimension(400, 300));
 		setLocationRelativeTo(null);
