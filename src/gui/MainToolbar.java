86491ec92432b81ac1f1213064bcefee4a94df3b
Andrej Gasic <andrej@gasic.rs>
2019-11-01 15:48:21 +0000

    Dodavanje prvobitnih ikonica na toolbar

--- src/gui/MainToolbar.java
+++ src/gui/MainToolbar.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
@@ -10,10 +13,18 @@ public class MainToolbar extends JToolBar {
 		
 		JButton newBtn = new JButton();
 		newBtn.setToolTipText("Create a new document");
+		newBtn.setIcon(new ImageIcon("images/documentnew.png"));
 		add(newBtn);
 		
 		JButton openBtn = new JButton();
 		openBtn.setToolTipText("Open an existing document");
+		openBtn.setIcon(new ImageIcon("images/document64.png"));
 		add(openBtn);
+		
+		JButton saveBtn = new JButton();
+		saveBtn.setToolTipText("Save changes to the project");
+		saveBtn.setIcon(new ImageIcon("images/save64.png"));
+		//saveBtn.setIcon(new ImageIcon((new ImageIcon("images/save64.png")).getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT)));	 moguce resenje za prevelike ikonice
+		add(saveBtn);
 	}
 }
