a96a039fbc6ad445557fb7d7308ef34d7183b833
Andrej Gasic <andrej@gasic.rs>
2019-11-06 22:10:55 +0000

    Razmaci

--- src/gui/AboutDialog.java
+++ src/gui/AboutDialog.java
@@ -10,22 +10,20 @@ import javax.swing.JPanel;
 public class AboutDialog extends JDialog {	
 	public AboutDialog(MainFrame frame) {
 		super(frame, "About");
-		Dimension dim=new Dimension(30, 20);
-		JLabel manager=new JLabel("Menager tima: Stefan Budimac");
-		JLabel leader=new JLabel("Vodja tima: Andrej Gasic");
-		JLabel indexManager=new JLabel("Indeks: RN06-2018");
-		JLabel indexLeader=new JLabel("Indeks: RN02-2018");
+		
+		Dimension dim=new Dimension(50, 20);
+		JLabel manager=new JLabel("Stefan Budimac RN06-2018");
+		JLabel leader=new JLabel("Andrej Gasic RN02-2018");
 		manager.setSize(dim);
 		leader.setSize(dim);
-		indexManager.setSize(dim);
-		indexLeader.setSize(dim);
-		GridLayout layout=new GridLayout(2, 2);
+		
+		GridLayout layout=new GridLayout();
 		JPanel panel=new JPanel(layout);
 		panel.add(manager);
 		panel.add(leader);
-		panel.add(indexManager);
-		panel.add(indexLeader);
 		add(panel);
+		
+		setLocationRelativeTo(null);
 		setSize(new Dimension(400, 300));
 		setLocationRelativeTo(null);
 		setVisible(true);
