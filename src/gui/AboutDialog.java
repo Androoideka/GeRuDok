610d81039eb7e274ca491d270412b77ad00dddda
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-06 22:47:52 +0000

    Prepravljanje about dijaloga, dodavanje pocetne verzije layouta

--- src/gui/AboutDialog.java
+++ src/gui/AboutDialog.java
@@ -1,8 +1,11 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,20 +14,48 @@ public class AboutDialog extends JDialog {
 	public AboutDialog(MainFrame frame) {
 		super(frame, "About");
 		
+		JPanel gornjiPanel=new JPanel(new FlowLayout());
+		gornjiPanel.setPreferredSize(new Dimension(600, 50));
+	
 		Dimension dim=new Dimension(50, 20);
-		JLabel manager=new JLabel("Stefan Budimac RN06-2018");
-		JLabel leader=new JLabel("Andrej Gasic RN02-2018");
-		manager.setSize(dim);
-		leader.setSize(dim);
+		JLabel menadzer=new JLabel("Stefan Budimac RN06-2018");
+		JLabel vodja=new JLabel("Andrej Gasic RN02-2018");
+		menadzer.setPreferredSize(dim);
+		vodja.setPreferredSize(dim);
 		
-		GridLayout layout=new GridLayout();
-		JPanel panel=new JPanel(layout);
-		panel.add(manager);
-		panel.add(leader);
-		add(panel);
+		gornjiPanel.add(menadzer);
+		gornjiPanel.add(vodja);
 		
-		setLocationRelativeTo(null);
-		setSize(new Dimension(400, 300));
+		add(gornjiPanel, BorderLayout.NORTH);
+		
+		JPanel centralniPanel=new JPanel(new GridLayout(2, 2, 0, 10));
+		centralniPanel.setPreferredSize(new Dimension(600, 250));
+		
+		JLabel emailMenadzer=new JLabel("sbudimac618rn@raf.rs");
+		JLabel brTelefonaMenadzer=new JLabel("+381653552359");
+		JLabel emailVodja=new JLabel("agasic218rn@raf.rs");
+		JLabel brTelefonaVodja=new JLabel("+381628931552");
+		emailMenadzer.setPreferredSize(dim);
+		brTelefonaMenadzer.setPreferredSize(dim);
+		emailVodja.setPreferredSize(dim);
+		brTelefonaVodja.setPreferredSize(dim);
+		centralniPanel.add(emailMenadzer, 0, 0);
+		centralniPanel.add(brTelefonaMenadzer, 1, 0);
+		centralniPanel.add(emailVodja, 0, 1);
+		centralniPanel.add(brTelefonaVodja, 1, 1);
+		
+		add(centralniPanel, BorderLayout.CENTER);
+		
+		JPanel donjiPanel=new JPanel();
+		
+		JLabel timskaSlika = new JLabel(new ImageIcon("tim/tim_201_14.PNG"));
+		add(timskaSlika);
+		
+		donjiPanel.add(timskaSlika);
+		
+		add(donjiPanel, BorderLayout.SOUTH);
+		
+		setSize(new Dimension(600, 500));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
