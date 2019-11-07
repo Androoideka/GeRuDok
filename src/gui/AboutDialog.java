fa3d69a73b68314c6752c0a5bf26a3be8c09fb0d
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-07 01:41:47 +0000

    Uklanjanje greske vezane za default close operation u about dijalogu

--- src/gui/AboutDialog.java
+++ src/gui/AboutDialog.java
@@ -105,7 +105,6 @@ public class AboutDialog extends JDialog {
 		
 		add(centralniPanel, BorderLayout.CENTER);
 		
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setSize(new Dimension(500, 500));
 		setLocationRelativeTo(null);
 		setVisible(true);
