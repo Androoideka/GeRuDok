766fa747e3d73c08fe51007fd8465a7d99b93182
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-07 00:54:35 +0000

    Dodavanje workspace.tree paketa i svih klasa u njemu

---
+++ src/workspace/tree/WorkspaceTreeEditor.java
@@ -0,0 +1,16 @@
+package workspace.tree;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+public class WorkspaceTreeEditor extends DefaultTreeCellEditor implements ActionListener {
+
+	public WorkspaceTreeEditor(JTree arg0, DefaultTreeCellRenderer arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
