cbd2b89f3e57fe5d29979668a2770b2e821ccbdc
sbudimac <sbudimac@gmail.com>
2019-11-07 01:24:55 +0000

    Upotpunjavanje metoda u workspace.tree paketu

--- src/workspace/tree/WorkspaceTreeCellRendered.java
+++ src/workspace/tree/WorkspaceTreeCellRendered.java
@@ -1,7 +1,43 @@
 package workspace.tree;
 
+import java.awt.Component;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
+import model.workspace.Document;
+import model.workspace.Project;
+import model.workspace.Workspace;
+
 public class WorkspaceTreeCellRendered extends DefaultTreeCellRenderer {
-	
-}
+	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+           super.getTreeCellRendererComponent(tree, value, sel,expanded, leaf, row,hasFocus);
+              
+           if (value instanceof Workspace ) {
+               URL imageURL = getClass().getResource("");
+               Icon icon = null;
+               if (imageURL != null) {
+            	   icon = new ImageIcon(imageURL);
+               }
+               setIcon(icon);
+           } else if (value instanceof Project ) {
+               URL imageURL = getClass().getResource("");
+               Icon icon = null;
+               if (imageURL != null) {
+            	   icon = new ImageIcon(imageURL);
+               }
+               setIcon(icon);
+           } else if (value instanceof Document ) {
+               URL imageURL = getClass().getResource("");
+               Icon icon = null;
+               if (imageURL != null) {
+            	   icon = new ImageIcon(imageURL);
+               }
+               setIcon(icon);
+           }
+           return this;
+	}
+}  
--- src/workspace/tree/WorkspaceTreeEditor.java
+++ src/workspace/tree/WorkspaceTreeEditor.java
@@ -1,16 +1,36 @@
 package workspace.tree;
 
+import java.awt.Component;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
 
+import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellEditor;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class WorkspaceTreeEditor extends DefaultTreeCellEditor implements ActionListener {
 
+	private Object node;
+	private JTextField field=null;
 	public WorkspaceTreeEditor(JTree arg0, DefaultTreeCellRenderer arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
 	
+	public Component getTreeCellEditorComponent(JTree arg0, Object arg1, boolean arg2, boolean arg3, boolean arg4, int arg5) {
+		node=arg1;
+		field=new JTextField(arg1.toString());
+		field.addActionListener(this);
+		return field;
+	}
+	
+	public boolean isCellEditable(EventObject arg0) {
+		if (arg0 instanceof MouseEvent)
+			if (((MouseEvent)arg0).getClickCount()==3){
+				return true;
+			}
+		return false;
+	}
 }
