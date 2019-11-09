991394ed907f9450ea55cb2048920ececd0e5ef8
Andrej Gasic <andrej@gasic.rs>
2019-11-09 20:04:06 +0000

    Eksperimentisanje sa look and feel

--- src/mehanickoPrebacivanje/Main.java
+++ src/mehanickoPrebacivanje/Main.java
@@ -1,9 +1,27 @@
 package mehanickoPrebacivanje;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import gui.MainFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (UnsupportedLookAndFeelException e) {
+			
+		}
+	    catch (ClassNotFoundException e) {
+	    	
+	    }
+	    catch (InstantiationException e) {
+	    	
+	    }
+	    catch (IllegalAccessException e) {
+	    	
+	    }
 		MainFrame glavniProzor = MainFrame.getInstance();
 	}
 }
