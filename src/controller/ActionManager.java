b0e0654bc6da01deb0f023aab9def2a859f96e8e
sbudimac <sbudimac@gmail.com>
2019-11-09 19:15:29 +0000

    popunjavanje ActionManager klase

--- src/controller/ActionManager.java
+++ src/controller/ActionManager.java
@@ -1,5 +1,21 @@
 package controller;
 
+
 public class ActionManager {
 	private AboutActionListener aboutActionListener;
+	
+	private NewDocumentAction newDocumentAction;
+	
+	public ActionManager() {
+		aboutActionListener=new AboutActionListener();
+		newDocumentAction=new NewDocumentAction();
+	}
+	
+	public AboutActionListener getAboutActionListener() {
+		return aboutActionListener;
+	}
+	
+	public NewDocumentAction getNewProjectAction() {
+		return newDocumentAction;
+	}
 }
