7ff6365185646ea2d5a8c0d10f1305959bd53077
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-09 19:05:40 +0000

    Dodavanje pocetne implementacije new document

---
+++ src/controller/NewDocumentAction.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
+import gui.MainFrame;
+import model.workspace.Document;
+import model.workspace.Project;
+
+public class NewDocumentAction extends MehanickoPrebacivanjeAction {
+	
+	public NewDocumentAction() {
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(
+		        KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		putValue(SMALL_ICON, loadIcon("ikonice/documentnew.png", 24, 24));
+		putValue(NAME, "New document");
+		putValue(SHORT_DESCRIPTION, "Create a new document.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object p=MainFrame.getInstance().getWorkspaceTree().getLastSelectedPathComponent();
+		if (p  instanceof Project) {
+			Document d=new Document((Project)p, "document");
+			((Project)p).addDocument(d);
+			//SwingUtilities.updateComponentTreeUI(AppCore.getInstance().getWorkspaceTree());
+		}
+	}
+
+}
