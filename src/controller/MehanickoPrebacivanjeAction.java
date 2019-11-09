d94e9f12b556e33ce8592d643fc508b7427ffa6e
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-09 18:37:22 +0000

    Automatsko skaliranje ikonica

--- src/controller/MehanickoPrebacivanjeAction.java
+++ src/controller/MehanickoPrebacivanjeAction.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.net.URL;
 
 import javax.swing.AbstractAction;
@@ -9,8 +10,10 @@ import javax.swing.ImageIcon;
 
 public abstract class MehanickoPrebacivanjeAction extends AbstractAction {
 	public Icon loadIcon(String fileName, int width, int height){
-		if (fileName != null) {                      
-	        return new ImageIcon(fileName, width, height);
+		if (fileName != null) {     
+			ImageIcon originalIcon = new ImageIcon(fileName);
+			ImageIcon scaledIcon = new ImageIcon(originalIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+			return scaledIcon;
 	    } else {                                     
 	        System.out.println("Resource not found: " + fileName);
 	    }
