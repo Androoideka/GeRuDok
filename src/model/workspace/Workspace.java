3e8d28471194ac62719d160f2b1a5e63ffaf8797
sbudimac <sbudimac@gmail.com>
2019-11-10 17:57:12 +0000

    implementacija metoda IModelObservera u klasu Workspace

--- src/model/workspace/Workspace.java
+++ src/model/workspace/Workspace.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 import javax.swing.tree.TreeNode;
 
-public class Workspace implements TreeNode {
+import observer.IModelObserver;
+import observer.IViewObserver;
+
+public class Workspace implements TreeNode, IModelObserver {
 	private String name = "Workspace";
 	private List<Project> prj = new ArrayList<>();
+	private List<IViewObserver> viewObservers = new ArrayList<IViewObserver>();
 	
 	public Workspace() {
 		super();
@@ -69,5 +73,34 @@ public class Workspace implements TreeNode {
 	@Override
 	public String toString() {
 		return name;
+	}
+
+	@Override
+	public void addObserver(IViewObserver viewObserver) {
+		if(viewObserver==null) {
+			return;
+		}
+		if(this.viewObservers.contains(viewObserver)) {
+			return;
+		}
+		this.viewObservers.add(viewObserver);
+	}
+
+	@Override
+	public void removeObserver(IViewObserver viewObserver) {
+		if(viewObserver==null || !viewObservers.contains(viewObserver)) {
+			return;
+		}
+		viewObservers.remove(viewObserver);
+	}
+
+	@Override
+	public void notifyObservers(Object event) {
+		if(event==null || viewObservers.isEmpty()) {
+			return;
+		}
+		for(IViewObserver viewObserver : viewObservers) {
+			viewObserver.update(event);
+		}
 	}
 }
