b86ff0bdbf353749b4f3d4747bec0a9295e69bc8
sbudimac <sbudimac@gmail.com>
2019-11-10 18:47:01 +0000

    brisanje nepotrebnog komentara u MehanickoPrebacivanjeAction klasi

--- src/controller/MehanickoPrebacivanjeAction.java
+++ src/controller/MehanickoPrebacivanjeAction.java
@@ -10,14 +10,6 @@ import helpers.ImageResizer;
 
 public abstract class MehanickoPrebacivanjeAction extends AbstractAction {
 	public Icon loadIcon(String fileName, int width, int height){
-		/*if (fileName != null) {     
-			ImageIcon originalIcon = new ImageIcon(fileName);
-			ImageIcon scaledIcon = new ImageIcon(originalIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
-			return scaledIcon;
-	    } else {                                     
-	        System.out.println("Resource not found: " + fileName);
-	    }
-		return null;*/
 		return ImageResizer.getProperlyScaledImage(fileName, width, height);
 	}
 }
