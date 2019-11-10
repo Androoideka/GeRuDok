0fb6bcf8062b6d6c6a941210751382b6543d37bb
sbudimac <sbudimac@gmail.com>
2019-11-10 21:53:07 +0000

    zamena JTabbedPane sa WorkspaceTabbedMenu

--- src/gui/WorkspaceTabbedMenu.java
+++ src/gui/WorkspaceTabbedMenu.java
@@ -2,14 +2,19 @@ package gui;
 
 import javax.swing.JTabbedPane;
 
+import observer.IModelObserver;
 import observer.IViewObserver;
 
 public class WorkspaceTabbedMenu extends JTabbedPane implements IViewObserver {
+	public WorkspaceTabbedMenu() {
+		super();
+	}
 
 	@Override
 	public void update(Object event) {
-		// TODO Auto-generated method stub
-		
+		if(event instanceof IModelObserver) {
+			IModelObserver obs = (IModelObserver)event;
+			obs.addObserver(this);
+		}
 	}
-
 }
