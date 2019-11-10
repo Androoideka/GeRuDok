b817b29910a0302b21c61469d8a58aa57d5a5657
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-10 01:21:16 +0000

    Vracanje actionlistener u TreeCellEditor klasu

--- src/workspace/tree/WorkspaceTreeEditor.java
+++ src/workspace/tree/WorkspaceTreeEditor.java
@@ -1,6 +1,7 @@
 package workspace.tree;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JTextField;
@@ -8,6 +9,9 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellEditor;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
+import model.workspace.Document;
+import model.workspace.Project;
+import model.workspace.Workspace;
 
 public class WorkspaceTreeEditor extends DefaultTreeCellEditor implements ActionListener {
 
@@ -22,5 +26,17 @@ public class WorkspaceTreeEditor extends DefaultTreeCellEditor implements Action
 		field=new JTextField(node.toString());
 		field.addActionListener(this);
 		return field;
+	}
+	
+	public void actionPerformed(ActionEvent event){
+        if (node instanceof Workspace) {
+                ((Workspace)node).setName(event.getActionCommand());
+        }
+        else if (node instanceof Project) {
+                ((Project)node).setName(event.getActionCommand());
+        }
+        else if(node instanceof Document) {
+                ((Document)node).setName(event.getActionCommand());
+        }
 	}
 }
