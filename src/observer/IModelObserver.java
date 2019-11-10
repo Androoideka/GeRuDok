74aa3d5bdcbd23d630cb427b7ef4121d5af4c424
sbudimac <sbudimac@gmail.com>
2019-11-10 17:48:43 +0000

    dodavanje metoda u observer interfejse

--- src/observer/IModelObserver.java
+++ src/observer/IModelObserver.java
@@ -1,5 +1,7 @@
 package observer;
 
 public interface IModelObserver {
-
+	void addObserver(IViewObserver viewObserver);
+	void removeObserver(IViewObserver viewObserver);
+	void notifyObservers(Object event);
 }
