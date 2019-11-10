1149cea20be8990747ff70ab3272b388a772bf7a
sbudimac <sbudimac@gmail.com>
2019-11-10 16:38:39 +0000

    Dodavanje tabova u akciji NewProjectAction

--- src/controller/NewProjectAction.java
+++ src/controller/NewProjectAction.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
@@ -11,6 +12,8 @@ import model.workspace.Project;
 import model.workspace.Workspace;
 
 public class NewProjectAction extends MehanickoPrebacivanjeAction {
+	private static int br=1;
+	
 	public NewProjectAction() {
 		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
 		putValue(SMALL_ICON, loadIcon("ikonice/projectnew.png", 16, 16));
@@ -28,6 +31,8 @@ public class NewProjectAction extends MehanickoPrebacivanjeAction {
 			((Workspace)w).addProject(p);
 			SwingUtilities.updateComponentTreeUI(MainFrame.getInstance().getWorkspaceTree());
 			//ovo bi mozda trebalo da se resava preko observera
+			MainFrame.getInstance().getTabbedPane().addTab("Projekat "+br, new JPanel());
+			br++;
 		}
 	}
 }
