dbc4ff2b43c51f2e8a4eda27d8412d9c6e2a07b0
sbudimac <sbudimac@gmail.com>
2019-11-11 21:36:48 +0000

    dodavanje opcije new project iz menu bar

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -2,6 +2,7 @@ package gui;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import controller.ActionManager;
 
@@ -10,6 +11,14 @@ public class MainMenu extends JMenuBar {
 		JMenu file = new JMenu("File");
 		JMenu edit = new JMenu("Edit");
 		JMenu help = new JMenu("Help");
+		
+		JMenu fileNew = new JMenu("New");
+		JMenuItem newProject = new JMenuItem("Project");
+		
+		file.add(fileNew);
+		
+		fileNew.add(newProject);
+		
 		
 		file.add(ActionManager.getInstance().getOpenWorkspaceAction());
 		
