a1e8454a1498eef7c0e3b1bf6b70fd20b58c2928
RN02-2018 <RN02-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-11-11 15:18:32 +0000

    Menjanje fonta na nivou aplikacije preko UIManager

--- src/mehanickoPrebacivanje/Main.java
+++ src/mehanickoPrebacivanje/Main.java
@@ -6,6 +6,7 @@ import javax.swing.Icon;
 import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.FontUIResource;
 
 import gui.MainFrame;
 import helpers.ImageResizer;
@@ -27,9 +28,23 @@ public class Main {
 	    catch (IllegalAccessException e) {
 	    	
 	    }*/
+		System.setProperty("awt.useSystemAAFontSettings","on");
+		System.setProperty("swing.aatext", "true");
+		
+		java.util.Enumeration<Object> keys = UIManager.getDefaults().keys();
+	    while (keys.hasMoreElements()) {
+	      Object key = keys.nextElement();
+	      Object value = UIManager.get (key);
+	      if (value instanceof javax.swing.plaf.FontUIResource) {
+	    	  FontUIResource oldF = (FontUIResource)value;
+	    	  FontUIResource f = new javax.swing.plaf.FontUIResource("Segoe UI",oldF.getStyle(),oldF.getSize() + 2);
+	    	  UIManager.put (key, f);
+	      }
+	    }
+	    
 		UIManager.put("Tree.collapsedIcon", ImageResizer.getProperlyScaledImage("ikonice/chevron-right.png", 10, 10));
 		UIManager.put("Tree.expandedIcon", ImageResizer.getProperlyScaledImage("ikonice/chevron-down.png", 10, 10));
-		UIManager.put("MenuItem.acceleratorFont", new Font("Cambria", Font.ITALIC, 8));
+		UIManager.put("MenuItem.acceleratorFont", new Font("Cambria", Font.ITALIC, 10));
 		MainFrame glavniProzor = MainFrame.getInstance();
 	}
 }
