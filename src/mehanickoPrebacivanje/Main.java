5b809748091f2f60c2a624a2ca1407af09068b3b
Andrej Gasic <andrej@gasic.rs>
2019-11-11 03:42:38 +0000

    Menjanje jtree ikonica

--- src/mehanickoPrebacivanje/Main.java
+++ src/mehanickoPrebacivanje/Main.java
@@ -1,14 +1,18 @@
 package mehanickoPrebacivanje;
 
+import java.awt.Font;
+
+import javax.swing.Icon;
+import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import gui.MainFrame;
+import helpers.ImageResizer;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		/*try {
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		}
 		catch (UnsupportedLookAndFeelException e) {
@@ -22,7 +26,10 @@ public class Main {
 	    }
 	    catch (IllegalAccessException e) {
 	    	
-	    }
+	    }*/
+		UIManager.put("Tree.collapsedIcon", ImageResizer.getProperlyScaledImage("ikonice/chevron-right.png", 10, 10));
+		UIManager.put("Tree.expandedIcon", ImageResizer.getProperlyScaledImage("ikonice/chevron-down.png", 10, 10));
+		UIManager.put("MenuItem.acceleratorFont", new Font("Cambria", Font.ITALIC, 8));
 		MainFrame glavniProzor = MainFrame.getInstance();
 	}
 }
