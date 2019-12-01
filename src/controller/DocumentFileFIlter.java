d0b3eb3f344313cc99b116914651fae06dbb11d5
sbudimac <sbudimac@gmail.com>
2019-12-01 14:42:46 +0000

    implementiranje klase OpenProjectAction i dodavanje klase DocumentFileFilter

---
+++ src/controller/DocumentFileFIlter.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class DocumentFileFIlter extends FileFilter {
+
+	@Override
+	public boolean accept(File f) {
+		return (f.isDirectory() || f.getName().toLowerCase().endsWith(".mp"));
+	}
+
+	@Override
+	public String getDescription() {
+		return "Mehanicko Prebacivanje (*.mp)";
+	}
+	
+}
--- src/controller/OpenProjectAction.java
+++ src/controller/OpenProjectAction.java
@@ -2,8 +2,18 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
+import javax.swing.JFileChooser;
 import javax.swing.KeyStroke;
+
+import gui.MainFrame;
+import model.workspace.Document;
+import model.workspace.Project;
+import model.workspace.Workspace;
 
 public class OpenProjectAction extends MehanickoPrebacivanjeAction {
 
@@ -17,7 +27,30 @@ public class OpenProjectAction extends MehanickoPrebacivanjeAction {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileFilter(new DocumentFileFIlter());
+		if(jfc.showOpenDialog(MainFrame.getInstance())==JFileChooser.APPROVE_OPTION) {
+			try {
+				ObjectInputStream ois=new ObjectInputStream(new FileInputStream(jfc.getSelectedFile()));
+				Project p=null;
+				try {
+					p=(Project)ois.readObject();
+				}catch(ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				Workspace ws=(Workspace)MainFrame.getInstance().getWorkspaceModel().getRoot();
+				ws.insert(p, ws.getChildCount());
+				for(int i=0;i<p.getChildCount();i++) {
+					Document d=new Document(p, "document");
+					p.insert(d, p.getChildCount());
+				}
+				ois.close();
+			}catch(FileNotFoundException e2) {
+				e2.printStackTrace();
+			}catch(IOException e3) {
+				e3.printStackTrace();
+			}
+		}
 	}
 
 }
