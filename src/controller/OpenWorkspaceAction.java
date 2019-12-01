d7ea49ddd501cc391964263a6358af0da6c91fa9
sbudimac <sbudimac@gmail.com>
2019-12-01 18:42:16 +0000

    implementiranje klase OpenWorkspaceAction

--- src/controller/OpenWorkspaceAction.java
+++ src/controller/OpenWorkspaceAction.java
@@ -2,8 +2,18 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
+import javax.swing.JFileChooser;
 import javax.swing.KeyStroke;
+
+import gui.MainFrame;
+import model.workspace.Document;
+import model.workspace.Project;
+import model.workspace.Workspace;
 
 public class OpenWorkspaceAction extends MehanickoPrebacivanjeAction {
 	
@@ -17,6 +27,28 @@ public class OpenWorkspaceAction extends MehanickoPrebacivanjeAction {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileFilter(new DocumentFileFIlter());
+		if(jfc.showOpenDialog(MainFrame.getInstance())==JFileChooser.APPROVE_OPTION) {
+			try {
+				ObjectInputStream ois=new ObjectInputStream(new FileInputStream(jfc.getSelectedFile()));
+				Workspace ws=null;
+				try {
+					ws=(Workspace)ois.readObject();
+				}catch(ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				ws=(Workspace)MainFrame.getInstance().getWorkspaceModel().getRoot();
+				for(int i=0;i<ws.getChildCount();i++) {
+					Project p=new Project(ws, "project");
+					ws.insert(p, ws.getChildCount());
+				}
+				ois.close();
+			}catch(FileNotFoundException e2) {
+				e2.printStackTrace();
+			}catch(IOException e3) {
+				e3.printStackTrace();
+			}
+		}
 	}
 }
