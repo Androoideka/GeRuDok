84be8d9f606458dc6f043e290249d20dc97aa97e
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-12-06 16:20:54 +0000

    zavrseno otvaranje dokumenta

--- src/controller/OpenAction.java
+++ src/controller/OpenAction.java
@@ -37,12 +37,13 @@ public class OpenAction extends AbstractAction {
 				}catch(ClassNotFoundException e1) {
 					e1.printStackTrace();
 				}
-				MPNode mpParent=MainFrame.getInstance().getWorkspaceTree().getRoot();
-				mpParent.setParent(mpParent);
-				for(int i=0;i<mpNode.getChildCount();i++) {
-					MPNode mpChild=(MPNode)mpNode.getChildAt(i);
-					mpNode.insert(mpChild, mpNode.getChildCount());
+				MPNode mpParent=(MPNode)mpNode.getParent();
+				if(mpParent==null) {
+					mpParent=MainFrame.getInstance().getWorkspaceTree().getRoot();
+					mpNode.setParent(mpParent);
 				}
+				mpNode.setParent(mpParent);
+				mpParent.insert(mpNode, mpParent.getChildCount());
 				ois.close();
 			}catch(FileNotFoundException e2) {
 				e2.printStackTrace();
