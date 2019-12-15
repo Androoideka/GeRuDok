d8eaa1ea34694556eeced6a6fe1476e377dbb68e
sbudimac <sbudimac@gmail.com>
2019-12-15 16:57:37 +0000

    dodavanje klase State

---
+++ src/state/State.java
@@ -0,0 +1,17 @@
+package state;
+
+import java.awt.event.MouseEvent;
+
+public class State {
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+}
