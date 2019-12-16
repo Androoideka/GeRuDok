47bcf980b66cf2cd1da1ecff8f904512e3c43034
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2019-12-16 15:12:04 +0000

    dodavanje ljusture za rescale i rotate state-ove

---
+++ src/state/RescaleState.java
@@ -0,0 +1,12 @@
+package state;
+
+import model.workspace.Document;
+
+public class RescaleState extends State {
+
+	public RescaleState(Document doc) {
+		super(doc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/state/RotateState.java
@@ -0,0 +1,12 @@
+package state;
+
+import model.workspace.Document;
+
+public class RotateState extends State {
+
+	public RotateState(Document doc) {
+		super(doc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
