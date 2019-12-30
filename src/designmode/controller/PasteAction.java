8407fa4c65a01e0cfe73a0a32c14950ffb4580d6
sbudimac <sbudimac@gmail.com>
2019-12-30 00:05:48 +0000

    implementacija klase PasteAction, metoda paste ima samo ljusturu za sad

---
+++ src/designmode/controller/PasteAction.java
@@ -0,0 +1,24 @@
+package designmode.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import helpers.ImageResizer;
+import view.MainFrame;
+
+public class PasteAction extends AbstractAction {
+	public PasteAction() {
+		putValue(ACCELERATOR_KEY,KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+		putValue(SMALL_ICON, ImageResizer.getInstance().loadSmallIcon("ikonice/paste.png"));
+		putValue(LARGE_ICON_KEY, ImageResizer.getInstance().loadBigIcon("ikonice/paste.png"));
+		putValue(SHORT_DESCRIPTION, "Paste copied or cut objects.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		MainFrame.getInstance().getWorkspaceTabbedMenu().getCurrentView().getCurrentView().paste();
+	}
+}
