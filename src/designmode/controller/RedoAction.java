36a12313a067bd63c5af012646b8cd15c23ae830
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2020-01-03 14:23:37 +0000

    dodate ikonice za undo i redo akcije

--- src/designmode/controller/RedoAction.java
+++ src/designmode/controller/RedoAction.java
@@ -1,11 +1,22 @@
 package designmode.controller;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import helpers.ImageResizer;
 
 public class RedoAction extends AbstractAction {
-
+	
+	public RedoAction() {
+		putValue(ACCELERATOR_KEY,KeyStroke.getKeyStroke(KeyEvent.VK_Y, 0)); //0 specifies no modifiers
+		putValue(SMALL_ICON, ImageResizer.getInstance().loadSmallIcon("ikonice/redo.png"));
+		putValue(LARGE_ICON_KEY, ImageResizer.getInstance().loadBigIcon("ikonice/redo.png"));
+		putValue(SHORT_DESCRIPTION, "Redo your last undone action.");
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
