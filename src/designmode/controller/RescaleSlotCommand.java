30df2bf9f28dbc8e309f2db2d3dc163c378c59ec
sbudimac <sbudimac@gmail.com>
2020-01-03 13:32:48 +0000

    implementacija klase RescaleSlotCommand

--- src/designmode/controller/RescaleSlotCommand.java
+++ src/designmode/controller/RescaleSlotCommand.java
@@ -1,5 +1,29 @@
 package designmode.controller;
 
-public class RescaleSlotCommand {
+import designmode.model.Handle;
+import document.model.Slot;
+
+public class RescaleSlotCommand extends AbstractCommand {
+	private Slot slot;
+	private Handle selectedHandle;
+	private double distanceX;
+	private double distanceY;
+	
+	public RescaleSlotCommand(Slot slot, Handle selectedHandle, double distanceX, double distanceY) {
+		this.slot=slot;
+		this.selectedHandle=selectedHandle;
+		this.distanceX=distanceX;
+		this.distanceY=distanceY;
+	}
+
+	@Override
+	public void doCommand() {
+		slot.scale(selectedHandle, distanceX, distanceY);
+	}
+
+	@Override
+	public void undoCommand() {
+		slot.scale(selectedHandle, -distanceX, -distanceY);
+	}
 
 }
