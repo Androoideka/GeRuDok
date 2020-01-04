2e839bc102289387e5c41c55e89af1863cbae4a8
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2020-01-04 19:28:50 +0000

    GUI za TextEditor(jos samo da se JTextPane size popravi)

--- src/designmode/view/TextEditor.java
+++ src/designmode/view/TextEditor.java
@@ -2,10 +2,16 @@ package designmode.view;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
 public class TextEditor extends Editor {
 	public TextEditor() {
 		super();
 		TextEditorToolbar tet=new TextEditorToolbar();
 		add(tet, BorderLayout.NORTH);
+		JTextPane tp=new JTextPane();
+		JScrollPane sp=new JScrollPane(tp);
+		add(sp, BorderLayout.SOUTH);
 	}
 }
