a38ab970c129c44fc42f0364604e64efa9247f2d
sbudimac <sbudimac@gmail.com>
2020-01-04 18:06:43 +0000

    pokusaj prepravke NullPointerException-a, promena velicina prozora i dodavanje ljustura za klase TextEditorToolbar i MultimedialEditorToolbar

--- src/designmode/view/Editor.java
+++ src/designmode/view/Editor.java
@@ -5,13 +5,13 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-public class Editor extends JFrame {
+public abstract class Editor extends JFrame {
 	
 	public Editor() {
 		setTitle("Slot content");
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = kit.getScreenSize();
-		setSize(new Dimension(screenSize.width/4, screenSize.height/4));
+		setSize(new Dimension(screenSize.width/2, screenSize.height/2));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
