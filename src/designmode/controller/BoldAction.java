d3402264bfcb939c36a143fdded4c72745b57680
RN06-2018 <RN06-2018@d1ad666b-af4c-e441-bd0c-f6bc5c3a8130>
2020-01-04 18:46:10 +0000

    dodat TextEditorToolbar

--- src/designmode/controller/BoldAction.java
+++ src/designmode/controller/BoldAction.java
@@ -1,5 +1,27 @@
 package designmode.controller;
 
-public class BoldAction {
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import helpers.ImageResizer;
+
+public class BoldAction extends AbstractAction {
+	
+	public BoldAction() {
+		putValue(ACCELERATOR_KEY,KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+		putValue(SMALL_ICON, ImageResizer.getInstance().loadSmallIcon("ikonice/bold.png"));
+		putValue(LARGE_ICON_KEY, ImageResizer.getInstance().loadBigIcon("ikonice/bold.png"));
+		putValue(NAME, "Bold");
+		putValue(SHORT_DESCRIPTION, "Bold selected text.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/designmode/controller/ChangeFontSizeAction.java
+++ src/designmode/controller/ChangeFontSizeAction.java
@@ -1,5 +1,27 @@
 package designmode.controller;
 
-public class ChangeFontSizeAction {
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import helpers.ImageResizer;
+
+public class ChangeFontSizeAction extends AbstractAction {
+	
+	public ChangeFontSizeAction() {
+		putValue(ACCELERATOR_KEY,KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
+		putValue(SMALL_ICON, ImageResizer.getInstance().loadSmallIcon("ikonice/fontsize.png"));
+		putValue(LARGE_ICON_KEY, ImageResizer.getInstance().loadBigIcon("ikonice/fontsize.png"));
+		putValue(NAME, "Font size");
+		putValue(SHORT_DESCRIPTION, "Change font size of selected text.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
