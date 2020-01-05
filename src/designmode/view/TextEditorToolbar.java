4e6b5be63850fb0f1b18b9c792d455ba3d17370f
sbudimac <sbudimac@gmail.com>
2020-01-05 19:18:03 +0000

    dodati comboBoxovi za velicinu i font

--- src/designmode/view/TextEditorToolbar.java
+++ src/designmode/view/TextEditorToolbar.java
@@ -1,6 +1,9 @@
 package designmode.view;
 
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.AbstractAction;
+import javax.swing.JComboBox;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -12,5 +15,15 @@ public class TextEditorToolbar extends JToolBar {
 		for(AbstractAction a : ActionManager.getInstance().getTextEditorActions()) {
 			add(a);
 		}
+		String[] fonts=new String[100];
+		fonts=GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		JComboBox<String> cbFont=new JComboBox<>(fonts);
+		add(cbFont);
+		Integer[] sizes=new Integer[76];
+		for(int i=0;i<76;i++) {
+			sizes[i]=i+2;
+		}
+		JComboBox<Integer> cbSize=new JComboBox<>(sizes);
+		add(cbSize);
 	}
 }
